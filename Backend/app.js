114f75fa0af3ac3dc7b99362b0a42ef9902276ba
paulamromero89 <134975205+paulamromero89@users.noreply.github.com>
2023-08-20 20:40:40 -0300

    Add files via upload
    Sprint IIII 
    Estructura inicial de nuestro servidor

---
+++ Backend/app.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+
+const PORT = 3000;
+
+/* app.use(express.static("public")); */
+
+app.listen(PORT, () => console.log("listening on port: ", PORT));
+
+//RUTAS-------------//
+
+/*  app.set("view engine","ejs"); */
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "views/index.html"));
+});
+
+app.get("/contacto", (req, res) => {
+res.sendFile(path.join(__dirname, "views/contacto.html"))
+}); 
+
+app.get("/carrito", (req, res) => {
+res.sendFile(path.join(__dirname, "views/cart.html"))
+}); 
+
+app.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname, "views/login.html"))
+  }); 
+  
+app.get("/registro", (req, res) => {
+  res.sendFile(path.join(__dirname, "views/registro.html"))
+  }); 
+
+  app.get("/producto", (req, res) => {
+    res.sendFile(path.join(__dirname, "views/productDetail.html"))
+    }); 
+  
