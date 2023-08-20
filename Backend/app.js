993aae7ccb0b99c0b550ddfbc0c3542a982a107b
paulamromero89 <134975205+paulamromero89@users.noreply.github.com>
2023-08-20 20:42:07 -0300

    Tp1 app.js

--- Backend/app.js
+++ Backend/app.js
@@ -2,16 +2,13 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
-
 const PORT = 3000;
 
-/* app.use(express.static("public")); */
+
 
 app.listen(PORT, () => console.log("listening on port: ", PORT));
 
-//RUTAS-------------//
 
-/*  app.set("view engine","ejs"); */
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "views/index.html"));
